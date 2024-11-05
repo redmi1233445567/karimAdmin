7351f7e86426c9b94a62eed4822e0e368229a8e8
redmi <hmadaredmi7@gmail.com>
2024-11-05 17:30:46 +0200

    edit collor

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-zinc-950`}
       >
         {children}
       </body>
